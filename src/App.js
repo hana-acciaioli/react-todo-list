8bad2f950aedf06f68c45e7b056752b5cd0cb076
Hana Acciaioli <hana.acciaioli@gmail.com>
2023-01-30 14:49:56 -0800

    added Routes to App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,25 @@ import Auth from './components/Auth/Auth.js';
 import Footer from './components/layout/Footer/Footer.js';
 import Header from './components/layout/Header/Header.js';
 import Todo from './components/Todo/Todo.js';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { useContext } from 'react';
+import { UserContext } from './context/UserContext';
 
 function App() {
+  const { user } = useContext(UserContext);
   return (
     <div className="App">
       <Header />
-      <Auth />
-      <Todo />
+      <Switch>
+        <Route path="/auth/:type" component={Auth} />
+        <Route path="/items" component={Todo} />
+        <Route exact path="/">
+          <>
+            {!user && <Redirect to="/auth/sign-in" />}
+            {user && <Redirect to="/items" />}
+          </>
+        </Route>
+      </Switch>
       <Footer />
     </div>
   );
