2c08483e93a048ad3dc5c8c4efc7285f1548ffbb
Hana Acciaioli <hana.acciaioli@gmail.com>
2023-01-30 12:35:10 -0800

    basic component structure built out

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Auth from './components/Auth/Auth.js';
+import Footer from './components/layout/Footer/Footer.js';
+import Header from './components/layout/Header/Header.js';
+import Todo from './components/Todo/Todo.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Auth />
+      <Todo />
+      <Footer />
     </div>
   );
 }
