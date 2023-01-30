cab58bfd6ddcd1006d1a1a48225623a96a0805a4
Hana Acciaioli <hana.acciaioli@gmail.com>
2023-01-30 14:22:53 -0800

    basic auth form setup and creating supabase user

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,5 +1,41 @@
 import React from 'react';
+import { useContext, useState } from 'react';
+import { UserContext } from '../../context/UserContext.js';
+import { useParams, NavLink } from 'react-router-dom';
+import { authUser } from '../../services/auth';
 
 export default function Auth() {
-  return <div>Auth</div>;
+  const { type } = useParams();
+  const { user, setUser } = useContext(UserContext);
+
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const submitAuthHandler = async () => {
+    try {
+      const user = await authUser(email, password, type);
+      setUser(user);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  return (
+    <div className="auth-container">
+      <nav>
+        <NavLink to="/auth/sign-up">Sign Up</NavLink>
+        <NavLink to="/auth/sign-in">Sign In</NavLink>
+        <div>
+          <label>Email</label>
+          <input value={email} onChange={(e) => setEmail(e.target.value)}></input>
+        </div>
+        <div>
+          <label>Password</label>
+          <input value={password} onChange={(e) => setPassword(e.targetValue)}></input>{' '}
+        </div>
+        <div>
+          <button onClick={submitAuthHandler}>Submit</button>
+        </div>
+      </nav>
+    </div>
+  );
 }
--- src/services/auth.js
+++ src/services/auth.js
@@ -3,3 +3,13 @@ import { client } from './client';
 export function getUser() {
   return client.auth.currentUser;
 }
+
+export async function authUser(email, password, type) {
+  let response = null;
+  if (type === 'sign-up') {
+    response = await client.auth.signUp({ email, password });
+  } else {
+    response = await client.auth.signIn({ email, password });
+  }
+  return response.user;
+}
