2a517f194f352d31439abbe026b41ba81866b149
Hana Acciaioli <hana.acciaioli@gmail.com>
2023-01-30 17:45:27 -0800

    new todo items adding to list

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { UserContext } from '../../context/UserContext.js';
 import { Redirect } from 'react-router-dom';
 import { useContext } from 'react';
+import { useTodos } from '../../hooks/useTodos.js';
+import { createTodo } from '../../services/todos.js';
+
 export default function Todo() {
   const { user } = useContext(UserContext);
+  const { todos, setTodos } = useTodos();
+  const [item, setItem] = useState('');
+
+  const newTodoHandler = async () => {
+    try {
+      await createTodo(item);
+      setTodos((oldList) => [...oldList, { item }]);
+      setItem('');
+    } catch (e) {
+      console.error(e.message);
+    }
+  };
 
   if (!user) {
     return <Redirect to="/auth/sign-in" />;
   }
-  return <div>Todo</div>;
+  return (
+    <>
+      <div>
+        {todos.map((todo) => (
+          <div key={todo.id}>
+            <input type="checkbox" checked={todo.complete} />
+            {todo.item}
+            {todo.id}
+          </div>
+        ))}
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="new item"
+          value={item}
+          onChange={(e) => setItem(e.target.value)}
+        ></input>
+        <button className="add-new-todo-button" onClick={newTodoHandler}>
+          Add
+        </button>
+      </div>
+    </>
+  );
 }
--- src/services/todos.js
+++ src/services/todos.js
@@ -4,3 +4,8 @@ export async function getTodos() {
   const resp = await client.from('todo').select('*');
   return checkError(resp);
 }
+
+export async function createTodo(item) {
+  const resp = await client.from('todo').insert({ item });
+  return checkError(resp);
+}
