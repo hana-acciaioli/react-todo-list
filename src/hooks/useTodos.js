f25edc9d199b4b0b2ed8a0ddc0e39a7687b1a538
Hana Acciaioli <hana.acciaioli@gmail.com>
2023-01-31 17:15:43 -0800

    removed todos from dependency array due to infinate loop

--- src/hooks/useTodos.js
+++ src/hooks/useTodos.js
@@ -1,8 +1,11 @@
 import { useState, useEffect } from 'react';
 import { getTodos } from '../services/todos.js';
+import { UserContext } from '../context/UserContext.js';
+import { useContext } from 'react';
 
 export function useTodos() {
   const [todos, setTodos] = useState([]);
+  const { user } = useContext(UserContext);
   useEffect(() => {
     const fetchTodos = async () => {
       try {
@@ -13,6 +16,7 @@ export function useTodos() {
       }
     };
     fetchTodos();
-  }, [todos]);
+  }, [user]);
+
   return { todos, setTodos };
 }
